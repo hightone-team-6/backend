8faff75ca8f0ff73c82e01d8833e1c99bb96f20c
prevw <contact@previousw.dev>
2025-02-15 23:32:05 +0900

    sigmaboy

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -35,7 +35,59 @@ export class LocationService {
           templateId: 3,
           locationId: 3,
           hashTags: ["#제주도", "#관광", "#여행"],
-        },
+        },{
+            imageUrls: Array(4).fill(
+              "https://i.namu.wiki/i/5FGUIiyTGl3EkaSlnnRnmoAsPBMkL8w1tVdj5pgDOoydk2T0brSqYsWyLgGqyELwn5oP8HWRhF8A-p8ZyN4FtQ.webp"
+            ),
+            title: "부산 맛집",
+            location: "부산광역시 해운대구",
+            description: "해운대 근처 맛집",
+            templateId: 2,
+            locationId: 2,
+            hashTags: ["#맛집", "#해운대", "#부산여행"],
+          },
+          {
+            imageUrls: Array(4).fill(
+              "https://i.namu.wiki/i/5FGUIiyTGl3EkaSlnnRnmoAsPBMkL8w1tVdj5pgDOoydk2T0brSqYsWyLgGqyELwn5oP8HWRhF8A-p8ZyN4FtQ.webp"
+            ),
+            title: "제주도 명소",
+            location: "제주특별자치도 서귀포시",
+            description: "제주 필수 관광지",
+            templateId: 3,
+            locationId: 3,
+            hashTags: ["#제주도", "#관광", "#여행"],
+          },{
+            imageUrls: Array(4).fill(
+              "https://i.namu.wiki/i/5FGUIiyTGl3EkaSlnnRnmoAsPBMkL8w1tVdj5pgDOoydk2T0brSqYsWyLgGqyELwn5oP8HWRhF8A-p8ZyN4FtQ.webp"
+            ),
+            title: "부산 맛집",
+            location: "부산광역시 해운대구",
+            description: "해운대 근처 맛집",
+            templateId: 2,
+            locationId: 2,
+            hashTags: ["#맛집", "#해운대", "#부산여행"],
+          },
+          {
+            imageUrls: Array(4).fill(
+              "https://i.namu.wiki/i/5FGUIiyTGl3EkaSlnnRnmoAsPBMkL8w1tVdj5pgDOoydk2T0brSqYsWyLgGqyELwn5oP8HWRhF8A-p8ZyN4FtQ.webp"
+            ),
+            title: "제주도 명소",
+            location: "제주특별자치도 서귀포시",
+            description: "제주 필수 관광지",
+            templateId: 3,
+            locationId: 3,
+            hashTags: ["#제주도", "#관광", "#여행"],
+          },{
+            imageUrls: Array(4).fill(
+              "https://i.namu.wiki/i/5FGUIiyTGl3EkaSlnnRnmoAsPBMkL8w1tVdj5pgDOoydk2T0brSqYsWyLgGqyELwn5oP8HWRhF8A-p8ZyN4FtQ.webp"
+            ),
+            title: "제주도 명소",
+            location: "제주특별자치도 서귀포시",
+            description: "제주 필수 관광지",
+            templateId: 3,
+            locationId: 3,
+            hashTags: ["#제주도", "#관광", "#여행"],
+          },
     ]
     
     getLocations() {
